926f1a2d56b09956d6478a780814417f35d4c86c
karaage0703 <coldchicken@gmail.com>
2016-04-23 14:49:40 +0900

    refactoring

--- nyamazu.sh
+++ nyamazu.sh
@@ -1,37 +1,20 @@
 #/bin/sh
 
-# define commands
-FIND='/usr/bin/find'
-XARGS='/usr/bin/xargs'
-
-## Mac OS X / Linux(Ubuntu) 
-if [ -f /bin/grep ]; then
-    GREP='/bin/grep'
-elif [ -f /usr/bin/grep ]; then
-    GREP='/usr/bin/grep'
-fi
-
-if [ -f /bin/basename ]; then
-    BASENAME='/bin/basename'
-elif [ -f /usr/bin/basename ]; then
-    BASENAME='/usr/bin/basename'
-fi
-
 # Get arguments 
-PLACE=$1
-TARGET=$2
+SEARCH_PLACE=$1
+SEARCH_WORD=$2
 
 # Display how to use
 function usage() {
-    NAME=`$BASENAME $0`
+    NAME=`basename $0`
     echo $NAME search_place search_word
 }
 
-# How to use if no argument
-if [ "$TARGET" == '' ]; then
+# Display usage if argument is wrong
+if [ "$SEARCH_WORD" == '' ]; then
     usage
     exit 1
 fi
-# Execute
 
-$FIND $PLACE -type f -print0 | $XARGS -0 $GREP -i -n "$TARGET"
+# Execute
+find $SEARCH_PLACE -type f -print0 | xargs -0 grep -i -n "$SEARCH_WORD"
