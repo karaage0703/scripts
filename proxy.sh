320a4dd57c99e4a04898cd71e8785faca8e521e6
karaage0703 <coldchicken@gmail.com>
2016-05-24 16:59:20 +0900

    more simple

--- proxy.sh
+++ proxy.sh
@@ -3,6 +3,6 @@
 ## How to use
 ## Execute source proxy.sh
 export http_proxy=http://proxy.example.com:8080/
-export https_proxy=http://proxy.example.com:8080/
-export HTTP_PROXY=http://proxy.example.com:8080/
-export HTTPS_PROXY=http://proxy.example.com:8080/
+export https_proxy=$http_proxy
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$http_proxy
